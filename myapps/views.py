1806e0dad827cc019d9f03b692dcf5991f54babb
Sazzat <asikulislamsazzat@gmail.com>
2023-02-21 09:42:31 +0600

    print_name_using_httpResponse

--- myapps/views.py
+++ myapps/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def print_response(request):
+    return HttpResponse("<h1>Hello I am Sazzat</h1>")
