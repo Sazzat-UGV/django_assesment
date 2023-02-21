215d28cba64c6dc5e5ad3f896701cc9a43651248
Sazzat <asikulislamsazzat@gmail.com>
2023-02-21 09:34:32 +0600

    register_apps_on_dashboard

--- myapps/models.py
+++ myapps/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name=models.CharField(max_length=200)
     details=models.TextField()
     price=models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return self.name
